88b0f18c768ec5df3691dd12f25122650d2932db
Brian.Mello@sentry.bdal.com <brian.mello@brukerdetection.us>
2019-05-24 22:09:08 -0400

    save

--- CartPole-v1/rl.py
+++ CartPole-v1/rl.py
@@ -46,4 +46,117 @@ class Memory():
                                size=batch_size, 
                                replace=False)
         return [self.buffer[ii] for ii in idx]
+
+def pretrain_memory(env, memory, state, pretrain_length=20):
+    # Make a bunch of random actions and store the experiences
+    for ii in range(pretrain_length):
+
+        # Make a random action
+        action = env.action_space.sample()
+        next_state, reward, done, _ = env.step(action)
+
+        if done:
+            # The simulation fails so no next state
+            next_state = np.zeros(state.shape)
+            # Add experience to memory
+            memory.add((state, action, reward, next_state))
+            
+            # Start new episode
+            env.reset()
+            # Take one random step to get the pole and cart moving
+            state, reward, done, _ = env.step(env.action_space.sample())
+        else:
+            # Add experience to memory
+            memory.add((state, action, reward, next_state))
+            state = next_state
+
+def train_and_save(env, sess, mainQN, memory, state, batch_size):
+
+    train_episodes = 1000          # max number of episodes to learn from
+    max_steps = 200                # max steps in an episode
+    gamma = 0.99                   # future reward discount
+
+    # Exploration parameters
+    explore_start = 1.0            # exploration probability at start
+    explore_stop = 0.01            # minimum exploration probability 
+    decay_rate = 0.0001            # exponential decay rate for exploration prob
+
+    # Initialize variables
+    sess.run(tf.global_variables_initializer())
+    
+    saver = tf.train.Saver()
+    rewards_list = []
+    step = 0
+    loss = None
+    for ep in range(1, train_episodes):
+        total_reward = 0
+        t = 0
+        while t < max_steps:
+            step += 1
+            # Uncomment this next line to watch the training
+            # env.render() 
+            
+            # Explore or Exploit
+            explore_p = explore_stop + (explore_start - explore_stop)*np.exp(-decay_rate*step) 
+            if explore_p > np.random.rand():
+                # Make a random action
+                action = env.action_space.sample()
+            else:
+                # Get action from Q-network
+                feed = {mainQN.inputs_: state.reshape((1, *state.shape))}
+                Qs = sess.run(mainQN.output, feed_dict=feed)
+                action = np.argmax(Qs)
+            
+            # Take action, get new state and reward
+            next_state, reward, done, _ = env.step(action)
+    
+            total_reward += reward
+            
+            if done:
+                # the episode ends so no next state
+                next_state = np.zeros(state.shape)
+                t = max_steps
+                
+                print('Episode: {}'.format(ep),
+                      'Total reward: {}'.format(total_reward),
+                      'Training loss: {:.4f}'.format(loss),
+                      'Explore P: {:.4f}'.format(explore_p))
+                rewards_list.append((ep, total_reward))
+                
+                # Add experience to memory
+                memory.add((state, action, reward, next_state))
+                
+                # Start new episode
+                env.reset()
+                # Take one random step to get the pole and cart moving
+                state, reward, done, _ = env.step(env.action_space.sample())
+
+            else:
+                # Add experience to memory
+                memory.add((state, action, reward, next_state))
+                state = next_state
+                t += 1
+            
+            # Sample mini-batch from memory
+            batch = memory.sample(batch_size)
+            states = np.array([each[0] for each in batch])
+            actions = np.array([each[1] for each in batch])
+            rewards = np.array([each[2] for each in batch])
+            next_states = np.array([each[3] for each in batch])
+            
+            # Train network
+            target_Qs = sess.run(mainQN.output, feed_dict={mainQN.inputs_: next_states})
+            
+            # Set target_Qs to 0 for states where episode ends
+            episode_ends = (next_states == np.zeros(states[0].shape)).all(axis=1)
+            target_Qs[episode_ends] = (0, 0)
+            
+            targets = rewards + gamma * np.max(target_Qs, axis=1)
+
+            loss, _ = sess.run([mainQN.loss, mainQN.opt],
+                                feed_dict={mainQN.inputs_: states,
+                                           mainQN.targetQs_: targets,
+                                           mainQN.actions_: actions})
+        
+    saver.save(sess, "checkpoints/cartpole.ckpt")
             
